c10f2006e95cf6a1ccfb5fa8faae0a5fbeaebdd9
dedyhutasoit7212640001-lgtm <dedyhutasoit.7212640001@mhs.unimed.ac.id>
2026-09-17 00:46:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,23 @@
-// Service Worker Dashboard Omset LBP B259
-// Strategi: cache-first untuk file inti (bisa dibuka offline setelah
-// pertama kali diinstal), selalu coba update dari jaringan di
-// background supaya versi terbaru terpasang saat online lagi.
-const CACHE_NAME = "dashboard-b259-v1";
+const CACHE_NAME = "dashboard-b259-v2";
 const CORE_FILES = [
-  "./index.html",
-  "./manifest.json",
-  "./icon-72.png",
-  "./icon-96.png",
-  "./icon-128.png",
-  "./icon-144.png",
-  "./icon-152.png",
-  "./icon-180.png",
-  "./icon-192.png",
-  "./icon-384.png",
-  "./icon-512.png"
+  "./index.html", "./manifest.json",
+  "./icon-72.png", "./icon-96.png", "./icon-128.png", "./icon-144.png",
+  "./icon-152.png", "./icon-180.png", "./icon-192.png", "./icon-384.png", "./icon-512.png"
 ];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_FILES)).then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_FILES)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
     caches.match(event.request).then((cached) => {
-      const networkFetch = fetch(event.request)
-        .then((res) => {
-          if (res && res.status === 200) {
-            const resClone = res.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
-          }
-          return res;
-        })
-        .catch(() => cached);
+      const networkFetch = fetch(event.request).then((res) => {
+        if (res && res.status === 200) { const c = res.clone(); caches.open(CACHE_NAME).then((cache) => cache.put(event.request, c)); }
+        return res;
+      }).catch(() => cached);
       return cached || networkFetch;
     })
   );
